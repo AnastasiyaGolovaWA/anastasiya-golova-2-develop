9fe9032b10096ea38fc755f18624238b70cd9ac1
Anastasiya.Golova <anastasiya.golova@cellpointmobile.com>
2022-07-23 23:42:08 +0400

    add cross origin

--- src/main/java/com/manager/rss/controllers/RssNewsController.java
+++ src/main/java/com/manager/rss/controllers/RssNewsController.java
@@ -3,11 +3,13 @@ package com.manager.rss.controllers;
 import com.manager.rss.service.dao.RssNewsInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/rssNews")
 public class RssNewsController {
     private RssNewsInterface RssNewsInterface;
