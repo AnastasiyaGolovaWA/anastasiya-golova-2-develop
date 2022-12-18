23d1fa2fa5572d471e179c72e077d36df5edd078
Anastasiya.Golova <Anastasiya.Golova@waveaccess.ru>
2022-12-18 14:51:48 +0400

    fix

--- src/test/com.manager.rss.test/TestMockito.java
+++ src/test/com.manager.rss.test/TestMockito.java
@@ -1,25 +1,28 @@
 package com.manager.rss.test;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.manager.rss.controllers.RssFeedController;
 import com.manager.rss.entity.RssFeed;
 import com.manager.rss.service.RssFeedService;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = RssFeedController.class)
 public class TestMockito {
@@ -29,6 +32,8 @@ public class TestMockito {
     private WebApplicationContext webApplicationContext;
     @MockBean
     private RssFeedService rssFeedService;
+
+    private static ObjectMapper mapper = new ObjectMapper();
 
     RssFeed rssFeed;
 
@@ -43,8 +48,23 @@ public class TestMockito {
     @Test
     public void testList() throws Exception {
         assertThat(this.rssFeedService).isNotNull();
-        mockMvc.perform(MockMvcRequestBuilders.get("/rssFeed/getAll"))
+        mockMvc.perform(get("/rssFeed/getAll"))
                 .andExpect(status().isOk())
                 .andDo(print());
+    }
+
+    @Test
+    public void testGetExample() throws Exception {
+        List<RssFeed> feeds = new ArrayList<>();
+        rssFeed = new RssFeed();
+        rssFeed.setIdRss(1L);
+        rssFeed.setUrl("url");
+        rssFeed.setCurrentPosition(true);
+        feeds.add(rssFeed);
+
+        Mockito.when(rssFeedService.getAll()).thenReturn(feeds);
+        mockMvc.perform(get("/rssFeed/getAll")).andExpect(status().isOk())
+                .andExpect(jsonPath("$", Matchers.hasSize(1)))
+                .andExpect(jsonPath("$[0].url", Matchers.equalTo("url")));
     }
 }
